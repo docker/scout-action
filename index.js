b9ceabaa70dcd513083610c4926ac6b622286468
brianru <1077086+brianru@users.noreply.github.com>
2026-05-19 12:53:24 +0000

    [BOT] Update assets for v1.21.0 release

--- index.js
+++ index.js
@@ -33984,7 +33984,7 @@ function requireSrc () {
 	}
 
 	async function main() {
-	    const version = "v1.20.4";
+	    const version = "v1.21.0";
 
 	    const binaryName = getBinaryName();
 	    const binaryPath = path.join(__dirname, "dist");
